9efe20ac0e4d6e4fe024d2d76a177c7fe7508ebb
Andrew Burnie <apburnie@hotmail.co.uk>
2022-11-11 15:36:13 +0000

    gives function list a title

--- apps/explorer/src/components/module/FunctionView.tsx
+++ apps/explorer/src/components/module/FunctionView.tsx
@@ -4,6 +4,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { useRpc } from '~/hooks/useRpc';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '~/ui/Tabs';
 
 function FunctionView({ pkgId }: { pkgId: string }) {
     const rpc = useRpc();
@@ -14,15 +15,32 @@ function FunctionView({ pkgId }: { pkgId: string }) {
 
     if (!!data) {
         return (
-            <div>
-                {Object.entries(data).map(([name, result]) => (
-                    <div
-                        key={name}
-                        className="bg-sui-grey-40 mb-2.5 px-5 py-4 rounded-lg text-body text-sui-grey-90"
-                    >
-                        <div className="font-semibold">{name}</div>
-                    </div>
-                ))}
+            <div className="border-0 md:border-l border-solid border-sui-grey-45 md:pl-7 pt-5 grow overflow-auto">
+                <TabGroup size="md">
+                    <TabList>
+                        <Tab>Simulate &amp; Execute</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <div className="overflow-auto h-verticalListLong">
+                                <div>
+                                    {Object.entries(data).map(
+                                        ([name, result]) => (
+                                            <div
+                                                key={name}
+                                                className="bg-sui-grey-40 mb-2.5 px-5 py-4 rounded-lg text-body text-sui-grey-90"
+                                            >
+                                                <div className="font-semibold">
+                                                    {name}
+                                                </div>
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            </div>
+                        </TabPanel>
+                    </TabPanels>
+                </TabGroup>
             </div>
         );
     }
